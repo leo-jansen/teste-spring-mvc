e3dc3ac309955710ca5a988dafd50fd781e950cb
leo-jansen <janseleo@gmail.com>
2021-09-18 23:08:37 -0300

    iniciando projeto

---
+++ src/main/java/com/example/spring/testespringmvc/TesteSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.testespringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringMvcApplication.class, args);
+	}
+
+}
