6e122c1276412c7347c3f31a85344210fc81c344
leo-jansen <janseleo@gmail.com>
2021-12-19 14:55:13 -0300

    habilitando cache na pagina home nas consultas de pedidos

--- src/main/java/com/example/spring/testespringmvc/repository/PedidoRepository.java
+++ src/main/java/com/example/spring/testespringmvc/repository/PedidoRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.example.spring.testespringmvc.model.Pedido;
 import com.example.spring.testespringmvc.model.StatusPedido;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -16,6 +17,7 @@ public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
   @Query("SELECT p FROM Pedido p JOIN p.usuario u WHERE p.status = :status AND u.username = :username")
   List<Pedido> findByStatusAndUsername(@Param("status") StatusPedido status, @Param("username") String username);
 
+  @Cacheable("pedidos")
   @Query("SELECT p FROM Pedido p JOIN p.usuario u WHERE p.status = :status")
   List<Pedido> findByStatus(@Param("status") StatusPedido status, Pageable page);
 
